190c1f00bd92cb45f3621e628d0a89c742d3ffdb
TimoHenderson <tjhenderson2@gmail.com>
2023-02-08 15:21:17 +0000

    added order template

--- controllers/controller.py
+++ controllers/controller.py
@@ -3,6 +3,11 @@ from app import app
 from models.order_list import order_list
 
 
-@app.route("/")
-def index():
+@app.route("/orders")
+def orders():
     return render_template("index.html", order_list=order_list)
+
+
+@app.route("/orders/<number>")
+def order(number):
+    return render_template("order.html", order=order_list[int(number)])
