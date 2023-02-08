226ffdcc685682b4f33e7e95be7a2e252ea84d88
TimoHenderson <tjhenderson2@gmail.com>
2023-02-08 14:46:24 +0000

    added order_list

---
+++ models/order_list.py
@@ -0,0 +1,8 @@
+from models.order import Order
+
+order_list = [
+    Order("12/03/22", "James Hetfield", "Microphone", 1),
+    Order("15/01/23", "Kirk Hammett", "Guitar", 5),
+    Order("07/06/22", "Robert Trujillo", "Bass Guitar", 1),
+    Order("23/02/21", "Lars Ulrich", "Drum Kit", 3),
+]
